5b3779bfff7003aa0df5be93120de78b9b18fb75
Renzo Caffesse <renzo.caffesse@climate.com>
2022-05-07 19:41:33 -0300

    Catalogo con rutas mas filtro por categoria

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom'
 
-const Item = ({ id, brand, model, specs, price }) => {
+const Item = ({ id, image, brand, model, specs, price }) => {
     return (
         <Link to={`/product/${id}`} className="h-full lg:w-1/5 sm:w-1/2 p-8">
             <div className="h-full flex relative">
-                <div className="px-4 py-10 relative w-full border-4 border-gray-200 bg-white opacity-50 hover:opacity-100 hover:cursor-pointer">
-                    {/* <span>
-                        <img src={image} alt={name} className={'small-img'} />
-                    </span> */}
+                <div className="px-4 py-10 relative w-full border-4 border-gray-200 bg-white opacity-70 hover:opacity-100 hover:cursor-pointer flex-col">
+                    <span className={'flex justify-center'}>
+                        <img src={image} alt={model} className={'w-36 justify-center'} />
+                    </span>
                     <p className="text-3xl">{brand}</p>
                     <p className="text-xl">Model: {model}</p>
                     <p className="text-lg">Specs: {specs}</p>
--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -2,20 +2,22 @@ import React from 'react';
 
 export const ItemDetail = ({
   id,
+  image,
   brand,
-  category,
   model,
   specs,
   price,
 }) => {
   return (
-    <div className="detail-row">
-      {/* <img src={image} alt={`${id}-${name}`} className="flex-col" /> */}
-      <div className="flex-col">
-        <h1>{brand}</h1>
-        <p>{model}</p>
-        <p>{specs}</p>
-        <h2>${price}</h2>
+    <div className="w-screen flex">
+      <div className="w-96 flex justify-center items-center">
+        <div className="w-full px-8">
+          <img src={image} alt={`${id}-${model}`} className={'w-64'} />
+          <h1>{brand}</h1>
+          <p>{model}</p>
+          <p>{specs}</p>
+          <h2>${price}</h2>
+        </div>
       </div>
     </div>
   );
